c46483e12ad5153705dea0ab30709dec00c8df49
gparihar <121501358+gparihar@users.noreply.github.com>
2026-06-05 15:11:00 +0530

    Netflix Clone completed

---
+++ script.js
@@ -0,0 +1,9 @@
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+
+    answer.classList.toggle("active");
+  });
+});
